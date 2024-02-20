febe01819d87c10a2ff668c4ab6dc1d41da8b88d
dilipsapk <dilip.sapkota@simfoni.com>
2024-02-20 10:51:45 +0530

    first commit

---
+++ 001_day_one.py
@@ -0,0 +1,63 @@
+"""
+Programming Language:
+#======================
+Programming language is a set of instructions to create communication human
+computer
+
+Python
+#======================
+Python is a
+    high level,        => It is easy to read and understand, looks like English or Mathematics
+    intrepreted,       => Executes codes line by line on the fly
+    dynamically typed, => No need to define data types, Python understands itself
+    object oriented supporting,   => Programming technique based on classes and objects
+    general purpose,   => It can be used in web development, scripting, AI/ML an scientific calculation
+    programming language.
+    
+Basic Data types in python
+===========================
+1] int:
+    example:
+        number = 100, marks = 679 
+2] str:
+    Three ways to create string data in python
+    using single quote:
+        Example:
+            name = 'Dilip Sapkota'
+    using double quote:
+        Example:
+            designation = "Django developer"
+    using triple quote:
+        Example:
+            info = '''My name is Dilip Sapkota. I am working in industry from eighteen years'''
+
+3] list:
+    creating list
+    Example:
+    numbers = []            =>created empty list using square bracket
+    numbers = list()        => Created empty list using list() class
+    marks = [84, 60, 33,90] =>Create list with values
+    tuple_info = tuple("My name is Dilip Sapkota.") => creates a list considering all character in string as single element 
+
+
+4] tuple:
+    Creating tuple:
+    Example:
+    tup_numbers = (1,2,3,4,5,6,7,8,9)
+    empty_tuple = tuple()   => Creates an empty tuple using tuple()
+    empty_tuple = ()      => creating an empty tuple using `()`
+    marks = 100, 89, 87, 78 => Creating tuple without parenthesis
+    tuple_single_element = (100,) =>creting a tuple with single element,comma is mandetory for single element otherwise it will be int
+    tuple_single_element = 100,   => Creating a single element tuple without parenthesis
+    tuple_info = tuple("My name is Dilip Sapkota.") => creates a tuple considering all character in string as single element
+    tuple_one = 1,2,3,4
+    tuple_two = "Five", "Six", "Seven", "Eight"
+    ocombined_tuple = (tuple_one,tuple_two)     => gives : ((1, 2, 3, 4), ('Five', 'Six', 'Seven', 'Eight'))
+    
+
+5]   
+    
+    
+"""
+list_to_tuple = tuple([1,2,3,4,5,6,7])
+print(list_to_tuple)
